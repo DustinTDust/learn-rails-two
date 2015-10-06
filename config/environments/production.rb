552b15277aacad7f074ed860bb6a3caf7856e218
Chad Ebbitt <chad.ebbitt@80stees.com>
2015-10-06 09:44:23 -0400

    email set ffor Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,21 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  #email enabled in production
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password,
+    authentication: "login",
+    enable_starttls_auto: true
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
